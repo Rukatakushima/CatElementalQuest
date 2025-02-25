79dc1f00467272b4ec6eb6606f83486e549d6f6d
Ануфриева Валерия 211-727 <broken.mind633@gmail.com>
2025-02-25 03:38:01 +0300

    Wind Ability #1
    - Created new prefab and script for Wind Ability: Wind Ability throws players up;
    - Changed Ability script: Ability Game Object isn't a chind object of Player, Player sets it active when uses Ability, Ability Collider triggers other elements;
    - Changed Ability and Player prefabs;

--- Assets/Scripts/Online/MenuManager.cs
+++ Assets/Scripts/Online/MenuManager.cs
@@ -27,6 +27,6 @@ public class MenuManager : MonoBehaviourPunCallbacks
 
     public override void OnJoinedRoom()
     {
-        PhotonNetwork.LoadLevel("Lobby");
+        PhotonNetwork.LoadLevel("Game");
     }
 }
--- Assets/Scripts/Players/Abilities/WindAbility.cs
+++ Assets/Scripts/Players/Abilities/WindAbility.cs
@@ -2,8 +2,20 @@ using UnityEngine;
 
 public class WindAbility : Ability
 {
+    [SerializeField] private float windForce = 10f;
+    [SerializeField] private Vector2 windDirection = Vector2.up;
+
     public override void UseAbility()
     {
-        Debug.Log("Воздух: Создаю ветрянной поток!");
+        Debug.Log("Воздух: Создаю ветряной поток!");
+    }
+
+    public void ApplyWindForce(Rigidbody2D targetRigidbody)
+    {
+        if (targetRigidbody != null)
+        {
+            targetRigidbody.AddForce(windDirection * windForce, ForceMode2D.Impulse);
+            Debug.Log($"Применена сила ветра к {targetRigidbody.gameObject.name}");
+        }
     }
 }
